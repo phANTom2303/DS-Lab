6de435c976d2e15810acf8eec549e60ee27c94b1
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-02 20:11:56 +0530

    Completed Lab 2.2 Square Matrix

---
+++ lab-2/1-2-squareMatrix.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    int n;
+    printf("Enter size of matrix : ");
+    scanf("%d",&n);
+
+    int **arr = (int**)malloc(n * sizeof(int*));
+    for(int i = 0; i < n; i ++)
+    {  
+        arr[i] = (int*)malloc(n*sizeof(int));
+    }
+
+    int count = 0;
+    printf("Enter matrix Elements : \n");
+    for(int i = 0; i < n; i ++)
+    {
+        for(int j = 0; j < n; j ++)
+        {
+            int input;
+            scanf("%d",&input);
+            arr[i][j] = input;
+
+            if(input > 0)
+                count ++;
+        }
+    }
+
+    printf("\nNumber of Non Zero Elements is : %d\n",count);
+    printf("\nUpper Triangular Matrix is : \n");
+    for(int i = 0; i < n; i ++)
+    {
+        for(int j = 0; j < n; j ++)
+        {
+            if(i <= j)
+                printf("%d",arr[i][j]);
+            else
+                printf(" ");
+            printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\nElements above and below the main diagonal : \n");
+    for(int i = 0; i < n; i ++)
+    {
+        for(int j = 0; j < n; j ++)
+        {
+            if(i != j)
+                printf("%d",arr[i][j]);
+            else
+                printf(" ");
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
