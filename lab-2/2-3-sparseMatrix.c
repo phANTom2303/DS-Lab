b29c735eacd78671271e5984fc0e6ebbea134264
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-23 17:40:49 +0530

    Updated 2.3 Sparse matrix with row, col and total element count display

--- lab-2/2-3-sparseMatrix.c
+++ lab-2/2-3-sparseMatrix.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-void main()
-{
-    int row, col;
-    printf("\nEnter Size of sparse Matrix : ");
-    scanf("%d %d",&row,&col);
-    int** arr = (int**)malloc(row * sizeof(int*));
-    for(int i = 0; i < row; i ++)
-    {
-        arr[i] = (int*)malloc(col*sizeof(int));
-    }
+void main() {
+  int row, col;
+  printf("\nEnter Size of sparse Matrix : ");
+  scanf("%d %d", &row, &col);
+  int **arr = (int **)malloc(row * sizeof(int *));
+  for (int i = 0; i < row; i++) {
+    arr[i] = (int *)malloc(col * sizeof(int));
+  }
 
-    printf("\nEnter Elements of sparse Matrix : \n"); 
-    for(int i =0; i < row; i ++)
-    {
-        for(int j = 0; j < col; j++)
-        {
-            int input;
-            scanf("%d",&input);
-            arr[i][j] = input;
-        }
+  int total = 0;
+  printf("\nEnter Elements of sparse Matrix : \n");
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      int input;
+      scanf("%d", &input);
+      arr[i][j] = input;
+      if (arr[i][j] != 0)
+        total += 1;
     }
+  }
 
-    printf("\nSparse Matrix in 3-tuple Format : \n");
-    for(int i = 0; i < row; i ++)
-    {
-        for(int j = 0; j < col; j ++)
-        {
-            if(arr[i][j] > 0)
-                printf("%d %d %d \n",i,j,arr[i][j]);
-        }
+  printf("\nSparse Matrix in 3-tuple Format : \n");
+  printf("%d %d %d\n", row, col, total);
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      if (arr[i][j] > 0)
+        printf("%d %d %d \n", i, j, arr[i][j]);
     }
-    printf("\n");
-    free(arr);
+  }
+  printf("\n");
+  free(arr);
 }
